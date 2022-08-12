af050166fa2ffdc5d578f03b7a8913fe763d4765
CodingCas <casweeno2000@gmail.com>
2022-08-12 12:20:11 +0100

    completed

---
+++ scripts/deploy.ts
@@ -0,0 +1,18 @@
+import { ethers } from "hardhat";
+
+async function main() {
+  const Vault = await ethers.getContractFactory(
+    "Vault"
+  );
+  const vault = await Vault.deploy();
+  await vault.deployed();
+
+  console.log("Vault deployed to: ", vault.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
